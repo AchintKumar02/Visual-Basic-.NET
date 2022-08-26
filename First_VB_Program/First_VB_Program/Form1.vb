d6f54caa685754a9d925cebe2c4d8ac83c5c1772
akgauravarya <kumarachint2002@gmail.com>
2022-08-26 12:21:22 +0530

    firstq

---
+++ First_VB_Program/First_VB_Program/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub BtnGO_Click(sender As Object, e As EventArgs) Handles BtnGO.Click
+        MsgBox("Hello World")
+    End Sub
+    Private Sub VariableBtn_Click(sender As Object, e As EventArgs) Handles VariableBtn.Click
+        Dim stName_Of_Student As String
+        stName_Of_Student = "Achint Kumar"
+        MsgBox("Welcome " & stName_Of_Student & " On your First Visual basic program")
+
+        stName_Of_Student = "Gaurav Arya"
+        MsgBox("Welcome " & stName_Of_Student & " On your First Visual basic program")
+    End Sub
+End Class
